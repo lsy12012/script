c10ed154037d62137ccaf241cdc7e52cff350e9c
lsy12012 <dltkddudz@naver.com>
2016-06-16 00:30:54 +0900

    project
    베포파일 작성

---
+++ script project/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 00:25:10 2016
+
+@author: dltkd
+"""
+
+from distutils.core import setup
+
+setup(name = 'Accident',
+      version = '1.0',
+      py_modules = ['launcher'],
+)
